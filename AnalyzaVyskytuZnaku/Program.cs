be4f68c60da62e289734d230bce71c66c48c5f88
Sardriel <91335030+Sardriel@users.noreply.github.com>
2021-10-07 10:49:51 +0200

    Add project files.

---
+++ AnalyzaVyskytuZnaku/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AnalyzaVyskytuZnaku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //řetězec, který chceme analyzovat
+            string s = "Mount Everest";
+            s = s.ToLower();
+
+            //inicializace počítadel
+            int pocetSamohlasek = 0;
+            int pocetSouhlasek = 0;
+
+            //definice typů znaků
+            string samohlasky = "aeiouyáéěíóúůý";
+            string souhlasky = "bcčdďfghjklmnpqrřsštťvwxzž";
+
+            //hlavní cyklus
+            foreach (char c in s)
+            {
+                if (samohlasky.Contains(c))
+                    pocetSamohlasek++;
+                else
+                if (souhlasky.Contains(c))
+                    pocetSouhlasek++;
+            }
+            Console.WriteLine("Samohlásek: {0}", pocetSamohlasek);
+            Console.WriteLine("Souhlásek: {0}", pocetSouhlasek);
+            Console.WriteLine("Nepísemných znaků: {0}", s.Length - (pocetSamohlasek + pocetSouhlasek));
+        }
+    }
+}
